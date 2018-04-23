5650255ee897de138219368bc9b4d716115edf08
Tri Samsul R <trisamsul@gmail.com>
2018-04-23 23:36:11 +0700

    v1.0
    Finished v1.0 (BETA)

--- Counting.py
+++ Counting.py
@@ -8,44 +8,61 @@ from scipy.spatial.distance import euclidean
 import numpy as np
 import cv2
 
-ext_data = 0
-
-# define the CNN network
-# Here we are using 19 layer CNN -VGG19 and initialising it
-# with pretrained imagenet weights
-base_model = load_model("model.h5")
-
-# Extract features from an arbitrary intermediate layer
-# like the block4 pooling layer in VGG19
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('Feature_Extract').output)
-
-# load both image and preprocess it
-img_path_1 = 'datatest/34.jpg'
-img_path_2 = 'datatest/50.jpg'
-
-img_1 = image.load_img(img_path_1, target_size=(105, 105))
-x = image.img_to_array(img_1)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-img_2 = image.load_img(img_path_2, target_size=(105, 105))
-y = image.img_to_array(img_2)
-y = np.expand_dims(y, axis=0)
-y = preprocess_input(y)
-
-# get the features 
-features_1 = model.predict(x)
-features_2 = model.predict(y)
-
+# variable to save extracted data
 ext_data = []
-ext_data.append(features_1)
-ext_data.append(features_2)
+#glob_max_dist = []
 
-i=0
-while i < len(ext_data):
-    print(ext_data[i])
-    i+=1
+def uniqueCount(model_file, img_path, dir_data):
 
-dist = np.sum((features_2-features_1)**2) / 1.0e+06 
-print(img_path_1, img_path_2)
-print('Difference value: ',dist)
+    #status if same face found ('0' none, '1' found)
+    status = 0
+
+    # distance value
+    dist = 0.0
+    max_dist = 0.0
+    
+    # Load model
+    base_model = load_model(model_file)
+
+    # Extract features from an arbitrary intermediate layer
+    # like the block4 pooling layer in VGG19
+    model = Model(inputs = base_model.input, outputs = base_model.get_layer('Feature_Extract').output)
+
+    img = image.load_img(img_path, target_size=(105, 105))
+    x = image.img_to_array(img)
+    x = np.multiply(x, 1.0 / 255.0)
+    x = np.expand_dims(x, axis=0)
+    #x = preprocess_input(x)
+
+    #print(np.max(x))
+    
+    # get the features 
+    features = model.predict(x)
+
+    if ext_data:
+        i=0
+        while i < len(ext_data) and status == 0:
+            
+            dist = np.sum((features - ext_data[i])**2)
+            print('Distance value: ',dist)
+
+            if dist < 350.0:
+                status = 1
+
+            if max_dist < dist:
+                max_dist = dist
+            
+            i += 1        
+    else:
+        print('(Extracted feature data is empty)\n')
+        
+    ext_data.append(features)
+    #glob_max_dist.append(max_dist)
+
+    #print('Max Distance Value: ',max_dist)
+    
+    if status == 0:
+        return 1
+    else:
+        return 0
+                    
